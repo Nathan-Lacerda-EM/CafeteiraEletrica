1dcfab18c9c21e998628c30786fa479e494b5d32
Daniel-Silva-EM <daniel.silva@escolarmanager.com.br>
2020-05-22 13:46:33 -0300

    Cria steps para implementação de testes automatizados no nivel de integração

---
+++ CafeteiraEletrica.Teste/Steps/CicloDeProducaoDeCafeStep.cs
@@ -0,0 +1,57 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace CafeteiraEletrica.Teste
+{
+    [Binding]
+    public class CicloDeProducaoDeCafeStep
+    {
+        [Given(@"uma cafeteira elétrica em perfeito funcionamento")]
+        public void GivenUmaCafeteiraEletricaEmPerfeitoFuncionamento()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"abastecido com água o respectivo receptáculo")]
+        public void GivenAbastecidoComAguaORespectivoReceptaculo()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"uma jarra vazia acoplada para coleta do café")]
+        public void GivenUmaJarraVaziaAcopladaParaColetaDoCafe()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"o café pronto para o consumo")]
+        public void GivenOCafeProntoParaOConsumo()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"pressionada a opção preparar")]
+        public void WhenPressionadaAOpcaoPreparar()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"identificado que foi servido por completo")]
+        public void WhenIdentificadoQueFoiServidoPorCompleto()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"o café está pronto e mantido aquecido")]
+        public void ThenOCafeEstaProntoEMantidoAquecido()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"o cliclo de confecção do café e finalizado")]
+        public void ThenOClicloDeConfeccaoDoCafeEFinalizado()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
--- CafeteiraEletrica.Teste/Steps/EspecificacaoDaCafeteiraEletricaStep.cs
+++ CafeteiraEletrica.Teste/Steps/EspecificacaoDaCafeteiraEletricaStep.cs
@@ -6,6 +6,142 @@ namespace CafeteiraEletrica.Teste.Steps
     [Binding]
     public class EspecificacaoDaCafeteiraEletricaStep
     {
-       
+        [Given(@"uma fonte de água quente")]
+        public void GivenUmaFonteDeAguaQuente()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que a fonte não contém água")]
+        public void GivenQueAFonteNaoContemAgua()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"um recipiente de contenção")]
+        public void GivenUmRecipienteDeContencao()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que o recipiente não esteja acoplado")]
+        public void GivenQueORecipienteNaoEstejaAcoplado()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"um interface de usuario")]
+        public void GivenUmInterfaceDeUsuario()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que o preparo do café foi iniciado")]
+        public void GivenQueOPreparoDoCafeFoiIniciado()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"o preparo do café e interrompido")]
+        public void GivenOPreparoDoCafeEInterrompido()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"o preparo do café e iniciado")]
+        public void GivenOPreparoDoCafeEIniciado()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"o café pronto para consumo")]
+        public void GivenOCafeProntoParaConsumo()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"precionado o botão de inicio na interface de usuario")]
+        public void WhenPrecionadoOBotaoDeInicioNaInterfaceDeUsuario()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"o usuario precionar o botão de inicio")]
+        public void WhenOUsuarioPrecionarOBotaoDeInicio()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"o recipiente de conteção e extraido")]
+        public void WhenORecipienteDeContecaoEExtraido()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"o recipiente de conteção e devolvido")]
+        public void WhenORecipienteDeContecaoEDevolvido()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"comcluido o preparo do café")]
+        public void WhenComcluidoOPreparoDoCafe()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"identificado o consumido completo")]
+        public void WhenIdentificadoOConsumidoCompleto()
+        {
+            throw new PendingStepException();
+        }
+        
+        [When(@"identificado que ainda não foi consumido por completo")]
+        public void WhenIdentificadoQueAindaNaoFoiConsumidoPorCompleto()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"o preparo do café não e iniciado")]
+        public void ThenOPreparoDoCafeNaoEIniciado()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"o preparo do café e iniciado")]
+        public void ThenOPreparoDoCafeEIniciado()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"o preparo do café e interrompido")]
+        public void ThenOPreparoDoCafeEInterrompido()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"o preparo do café e retomado")]
+        public void ThenOPreparoDoCafeERetomado()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"mantido aquecido até ser consumo por completo")]
+        public void ThenMantidoAquecidoAteSerConsumoPorCompleto()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"o café está pronto para o consumo")]
+        public void ThenOCafeEstaProntoParaOConsumo()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"o ciclo de preparo e finalizado")]
+        public void ThenOCicloDePreparoEFinalizado()
+        {
+            throw new PendingStepException();
+        }
     }
 }
